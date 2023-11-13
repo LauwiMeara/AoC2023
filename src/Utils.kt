d0eeab7d358735933c786c761f80cca6c340ca59
Lauwi Meara <laurasusanne@live.nl>
2023-11-13 08:38:50 +0100

    Update utils

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,19 +1,32 @@
+@file:Suppress("unused")
+
 import java.io.File
-import java.math.BigInteger
-import java.security.MessageDigest
 
 /**
- * Reads lines from the given input txt file.
+ * Reads from the given input txt file as one string.
  */
 fun readInput(name: String) = File("src", "$name.txt")
+    .readText()
+
+/**
+ * Reads from the given input txt file as strings split by the given delimiter.
+ */
+fun readInputSplitByDelimiter(name: String, delimiter: String) = File("src", "$name.txt")
+    .readText()
+    .split(delimiter)
+
+/**
+ * Reads lines from the given input txt file as strings.
+ */
+fun readInputAsStrings(name: String) = File("src", "$name.txt")
     .readLines()
 
 /**
- * Converts string to md5 hash.
+ * Reads lines from the given input txt file as integers.
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+fun readInputAsInts(name: String) = File("src", "$name.txt")
+    .readLines()
+    .map{it.toInt()}
 
 /**
  * The cleaner shorthand for printing output.
